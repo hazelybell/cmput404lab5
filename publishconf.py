e372d4637d138f1270e513fdc05a96fb513bf66d
Hazel Victoria Campbell <github@hazel.zone>
2021-02-10 17:42:02 -0700

    second attempt: fix siteurl?

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://cmput404lab5.github.io/'
+SITEURL = 'https://hazelybell.github.io/cmput404lab5/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
